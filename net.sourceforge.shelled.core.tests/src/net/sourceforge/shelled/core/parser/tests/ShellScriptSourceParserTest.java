d1ef0791ec3be1892ba9aabf236bc0f5e0892fa7
Alexander Kurtakov <akurtakov@gmail.com>
2010-01-06 11:21:36 +0000

    Initial import.

---
+++ net.sourceforge.shelled.core.tests/src/net/sourceforge/shelled/core/parser/tests/ShellScriptSourceParserTest.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Kurtakov - initial API and implementation
+ *******************************************************************************/
+package net.sourceforge.shelled.core.parser.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShellScriptSourceParserTest {
+
+	@Test
+	public void testParse() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ net.sourceforge.shelled.core.tests/src/net/sourceforge/shelled/core/tests/AllTests.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Kurtakov - initial API and implementation
+ *******************************************************************************/
+package net.sourceforge.shelled.core.tests;
+
+
+import net.sourceforge.shelled.core.parser.tests.ParserSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  ParserSuite.class
+})
+public class AllTests {
+}
