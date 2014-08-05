bd8191af1bca0535a6696a69b5941f13cab9acf0
Peter Palaga <ppalaga@redhat.com>
2014-08-05 09:29:46 +0200

    Add folding preference page and make comments folding obey the preferences

---
+++ net.sourceforge.shelled.ui/src/net/sourceforge/shelled/ui/preferences/ShellFoldingPreferencePage.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Alexander Kurtakov - initial API and implementation
+ *******************************************************************************/
+package net.sourceforge.shelled.ui.preferences;
+
+import net.sourceforge.shelled.ui.Activator;
+import net.sourceforge.shelled.ui.text.folding.ShellFoldingPreferenceBlock;
+import net.sourceforge.shelled.ui.text.folding.ShellCommentFoldingPreferenceBlock;
+
+import org.eclipse.dltk.ui.preferences.AbstractConfigurationBlockPreferencePage;
+import org.eclipse.dltk.ui.preferences.IPreferenceConfigurationBlock;
+import org.eclipse.dltk.ui.preferences.OverlayPreferenceStore;
+import org.eclipse.dltk.ui.text.folding.DefaultFoldingPreferenceConfigurationBlock;
+import org.eclipse.dltk.ui.text.folding.IFoldingPreferenceBlock;
+import org.eclipse.jface.preference.PreferencePage;
+
+/**
+ * @author <a href="mailto:ppalaga@redhat.com">Peter Palaga</a>
+ */
+public class ShellFoldingPreferencePage extends
+		AbstractConfigurationBlockPreferencePage {
+	@Override
+	protected String getHelpId() {
+		return null;
+	}
+
+	@Override
+	protected void setDescription() {
+	}
+
+	@Override
+	protected void setPreferenceStore() {
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+	}
+
+	@Override
+	protected IPreferenceConfigurationBlock createConfigurationBlock(
+			OverlayPreferenceStore overlayPreferenceStore) {
+		return new DefaultFoldingPreferenceConfigurationBlock(
+				overlayPreferenceStore, this) {
+			@Override
+			protected IFoldingPreferenceBlock createDocumentationBlock(
+					OverlayPreferenceStore store, PreferencePage page) {
+				return new ShellCommentFoldingPreferenceBlock(store, page);
+			}
+
+			@Override
+			protected IFoldingPreferenceBlock createSourceCodeBlock(
+					OverlayPreferenceStore store, PreferencePage page) {
+				return new ShellFoldingPreferenceBlock(store, page);
+			}
+		};
+	}
+}
